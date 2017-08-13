1661755b2aa124b9172cae8defd74417fa2df26d
Wojtek Kokorzycki <ksiadz@users.noreply.github.com>
2017-08-13 13:43:40 +0200

    user is now inhiriting afrer CRUDMixin

--- pirateup/users/models.py
+++ pirateup/users/models.py
@@ -1,8 +1,7 @@
-from back.pirateup.data import db
+from back.pirateup.data import db, CRUDMixin
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class User(db.Model, CRUDMixin):
     cookie = db.Column(db.String, unique=True)
 
     def __init__(self, cookie):
