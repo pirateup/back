c798617ddceec0ce9f4237eb83a32784db6013ed
4fuss <a.kawczynski@gmail.com>
2017-08-24 22:59:44 +0200

    search by shop name api

--- pirateup/data.py
+++ pirateup/data.py
@@ -36,3 +36,16 @@ class CRUDMixin(object):
     def delete(self, commit=True):
         db.session.delete(self)
         return commit and db.session.commit()
+
+def query_to_list(query, include_field_names=True):
+    column_names = []
+    for i, obj in enumerate(query.all()):
+        if i == 0:
+            column_names = [c.name for c in obj.__table__.columns]
+            if include_field_names:
+                yield column_names
+        yield obj_to_list(obj, column_names)
+
+
+def obj_to_list(sa_obj, field_order):
+    return [getattr(sa_obj, field_name, None) for field_name in field_order]
--- pirateup/pirateup.py
+++ pirateup/pirateup.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
-from pirateup.store.models import Store
 from .data import db
+from .store.controllers import store
 import os
 import requests
 
@@ -9,23 +9,10 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+app.register_blueprint(store)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/shops', methods=['GET','POST'])
-def stores():
-    data = request.get_json()
-    print(data)
-    result = {}
-    if request.method == 'POST':
-        #ADD NEW STORE
-        store = Store.create(**data)
-        result = {'status': 'OK'}        
-    elif request.method == 'GET':
-        #RETURN ALL STORES
-        pass
-
-    return jsonify(status='OK'), 200 
