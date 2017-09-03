31ae2ebd22487880e2c095f48b5804d05577a556
4fuss <a.kawczynski@gmail.com>
2017-09-03 23:24:55 +0200

    search by tag and nearby apis

--- pirateup/store/models.py
+++ pirateup/store/models.py
@@ -1,6 +1,10 @@
 from pirateup.data import db, CRUDMixin 
 from geoalchemy2 import Geography
 
+store_tags = db.Table('store_tag_rel',
+        db.Column('store_id', db.Integer, db.ForeignKey('store.id')),
+        db.Column('tag_id', db.Integer, db.ForeignKey('store_tag.id')))
+
 class Store(db.Model, CRUDMixin):
     __tablename__ = 'store'
 
@@ -9,3 +13,11 @@ class Store(db.Model, CRUDMixin):
     street = db.Column(db.String())
     zip = db.Column(db.String())
     location = db.Column(Geography(geometry_type='POINT',srid=4326))
+    tags = db.relationship('StoreTag', secondary=store_tags, 
+            backref=db.backref('stores', lazy='dynamic'))
+
+
+class StoreTag(db.Model, CRUDMixin):
+    __tablename__ = 'store_tag'
+
+    name = db.Column(db.String())
